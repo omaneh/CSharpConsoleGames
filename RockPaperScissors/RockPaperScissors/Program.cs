18fc01efbaadeeec85b4757479b0b1b125121a2a
omaneh <122321976+omaneh@users.noreply.github.com>
2023-03-12 10:20:52 -0400

    add RockPaperScissors

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -3,15 +3,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-       for (int i = 0; i < 5; i++)
+        Game gameRound = new Game();
+
+       for (int i = 1; i < 5; i++)
         {
+            
             Console.WriteLine("-------------------------------");
             Console.WriteLine($"ROCK, PAPER, SCISSORS \nROUND: {i}");
             Console.Write("Player 1, please enter your choice for this round: ");
             string playerOneChoice = Console.ReadLine();
             playerOneChoice.ToLower();
-            GameOptions playerOneEnum;
 
 
             if (playerOneChoice == null)
@@ -19,23 +20,14 @@ class Program
                 Console.WriteLine("Please enter one of the following: rock, paper, or sicssor");
             }
 
-            if (playerOneChoice == "rock" || playerOneChoice == "rocks")
-            {
-                playerOneEnum = GameOptions.Rock;
-            } else if (playerOneChoice == "paper")
-            {
-                playerOneEnum = GameOptions.Paper;
-            } else
-            {
-                playerOneEnum = GameOptions.Scissor;
-            }
+
+            gameRound.PlayerOneSelect(playerOneChoice);
 
             //player two
 
             Console.Write("Player 2, please enter your choice for this round: ");
             string playerTwoChoice = Console.ReadLine();
             playerTwoChoice.ToLower();
-            GameOptions playerTwoEnum;
 
 
             if (playerTwoChoice == null)
@@ -43,25 +35,25 @@ class Program
                 Console.WriteLine("Please enter one of the following: rock, paper, or sicssor");
             }
 
-            if (playerTwoChoice == "rock")
-            {
-                playerTwoEnum = GameOptions.Rock;
-            }
-            else if (playerOneChoice == "paper")
-            {
-                playerTwoEnum = GameOptions.Paper;
-            }
-            else
-            {
-                playerTwoEnum = GameOptions.Scissor;
-            }
+            gameRound.PlayerTwoSelect(playerTwoChoice);
 
-            Game gameRound = new Game(playerOneEnum, playerTwoEnum);
-            gameRound.GameRound(playerOneEnum, playerTwoEnum);
+            gameRound.GameRound(gameRound._playerOneChoice, gameRound._playerTwoChoice);
 
             Console.WriteLine($"Player 1 selected {gameRound._playerOneChoice}. Player 2 selected {gameRound._playerTwoChoice}");
             Console.WriteLine($"The result of the round: {gameRound._resultOfGame}. \nPlayer 1 score: {gameRound._playerOneScore}. \nPlayer 2 score: {gameRound._playerTwoScore}.\nNumber of draws: {gameRound._totalDraws}.");
         }
+
+       if (gameRound._playerOneScore > gameRound._playerTwoScore)
+        {
+            Console.WriteLine($"Player 1 wins with a score of {gameRound._playerOneScore}");
+        } else if (gameRound._playerOneScore < gameRound._playerTwoScore)
+        {
+            Console.WriteLine($"Player 2 wins with a total score of {gameRound._playerTwoScore}");
+        } else
+        {
+            Console.WriteLine($"This game ended in a draw. There were {gameRound._totalDraws} total draws in the game");
+        }
+        Console.ReadKey();
 
        
     }
@@ -79,31 +71,30 @@ class Game
     internal int _playerTwoScore { get; set; } = 0;
     internal int _totalDraws { get; set; } = 0;
 
-    public Game(GameOptions playerOneChoice, GameOptions playerTwoChoice)
-    {
-        _playerOneChoice = playerOneChoice;
-        _playerTwoChoice = playerTwoChoice;
-    }
-
+    public Game() { }
     public void GameRound(GameOptions _playerOneChoice, GameOptions _playerTwoChoice)
     {
         if (_playerOneChoice == GameOptions.Rock && _playerTwoChoice == GameOptions.Rock || _playerOneChoice == GameOptions.Paper && _playerTwoChoice == GameOptions.Paper || _playerOneChoice == GameOptions.Scissor && _playerTwoChoice == GameOptions.Scissor)
         {
-            RoundScore(GameResult.Draw);
+            _resultOfGame = GameResult.Draw;
+             RoundScore(GameResult.Draw);
         }
         else if (_playerOneChoice == GameOptions.Rock && _playerTwoChoice == GameOptions.Paper || _playerOneChoice == GameOptions.Paper && _playerTwoChoice == GameOptions.Scissor || _playerOneChoice == GameOptions.Scissor && _playerTwoChoice == GameOptions.Rock)
         {
-            RoundScore(GameResult.PlayerTwoWin);
+            _resultOfGame = GameResult.PlayerTwoWin;
+             RoundScore(GameResult.PlayerTwoWin);
         }
         else 
         {
-            RoundScore(GameResult.PlayerOneWin);
+            _resultOfGame = GameResult.PlayerOneWin;
+             RoundScore(GameResult.PlayerOneWin);
         } 
         
     }
 
     public void RoundScore(GameResult _resultOfGame)
     {
+
         switch (_resultOfGame)
         {
             case GameResult.PlayerOneWin:
@@ -118,6 +109,47 @@ class Game
         }
     }
 
+    public GameOptions PlayerOneSelect(string playerOneChoice)
+    {
+        if (playerOneChoice == "rock")
+        {
+            _playerOneChoice = GameOptions.Rock;
+            return _playerOneChoice;
+        }
+        else if (playerOneChoice == "paper")
+        {
+            _playerOneChoice = GameOptions.Paper;
+            return _playerOneChoice;
+        }
+        else 
+        {
+            _playerOneChoice = GameOptions.Scissor;
+            return _playerOneChoice;
+        }
+
+    }
+
+    public GameOptions PlayerTwoSelect(string playerTwoChoice)
+    {
+        if (playerTwoChoice == "rock")
+        {
+            _playerTwoChoice = GameOptions.Rock;
+            return _playerTwoChoice;
+
+        }
+        else if (playerTwoChoice == "paper")
+        {
+            _playerTwoChoice = GameOptions.Paper;
+            return _playerTwoChoice;
+        }
+        else 
+        {
+            _playerTwoChoice = GameOptions.Scissor;
+            return _playerTwoChoice;
+        }
+    }
+
+  
 }
 public enum GameOptions
 {
